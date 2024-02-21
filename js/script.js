1a52fb5cd573251e34fbf13d467e26a27e062860
enrico-amerio <amerio.enrico95@gmail.com>
2024-02-21 17:41:01 +0100

    [add] Price calculation

--- js/script.js
+++ js/script.js
@@ -1,22 +1,30 @@
+const name = document.getElementById('fullName')
 const distance = document.getElementById('km');
 const ageRange = document.getElementById('age')
-console.log(distance.value, ageRange.value);
+console.log(fullName.value, distance.value, ageRange.value);
+
 
 const priceKm = 0.21;
-const fullPrice = distance * priceKm;
-// const discountYoung = 20;
-// const discountSenior = 40;
+const fullPrice = distance.value * priceKm;
+const discountYoung = 20;
+const discountSenior = 40;
 console.log(fullPrice);
 
 //ELEMENTS
 const btn1 = document.querySelector('.btn1');
 
 
-
-
-
+if(ageRange.value == 1){
+  risultato = fullPrice - ((fullPrice * discountYoung) / 100);
+}else if(ageRange.value == 3){
+  risultato = fullPrice - ((fullPrice * discountSenior) / 100);
+}else{
+  risultato = fullPrice;
+}
+console.log(risultato)
 
 
 btn1.addEventListener('click',function(event){
-  event.preventDefault();
+  event.submit();
+ 
 })
